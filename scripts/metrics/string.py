ee5ca56e54b42eb3e26fc9a4dcd3d933a280ab16
artemvaska <artem_vasilev_01@list.ru>
2024-03-20 17:12:25 +0300

    Specify download path in get_protein_links function

--- scripts/metrics/string.py
+++ scripts/metrics/string.py
@@ -1,7 +1,9 @@
-import numpy as np
+import os
 import gzip
 import shutil
 import wget
+
+import numpy as np
 import pandas as pd
 
 
@@ -12,15 +14,19 @@ def get_protein_links(taxon_id: str) -> pd.DataFrame:
     :param taxon_id: taxon_id for species
     :return: pd.DataFrame with protein combined scores
     """
+    output_dir = f"results/string_protein_links/{taxon_id}"
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
     suffix = "protein.links.v12.0"
     url = f"https://stringdb-downloads.org/download/protein.links.v12.0/{taxon_id}.{suffix}.txt.gz"
-    wget.download(url, bar=None)
+    wget.download(url, out=output_dir, bar=None)
 
-    with gzip.open(f"{taxon_id}.{suffix}.txt.gz", "rb") as in_file:
-        with open(f"{taxon_id}.{suffix}.txt", "wb") as out_file:
+    with gzip.open(f"{output_dir}/{taxon_id}.{suffix}.txt.gz", "rb") as in_file:
+        with open(f"{output_dir}/{taxon_id}.{suffix}.txt", "wb") as out_file:
             shutil.copyfileobj(in_file, out_file)
 
-    protein_links = pd.read_csv(f"{taxon_id}.{suffix}.txt", sep=" ")
+    protein_links = pd.read_csv(f"{output_dir}/{taxon_id}.{suffix}.txt", sep=" ")
     protein_links.protein1 = protein_links.protein1.str.replace(taxon_id + ".", "")
     protein_links.protein2 = protein_links.protein2.str.replace(taxon_id + ".", "")
 
